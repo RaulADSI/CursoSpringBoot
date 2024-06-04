ed224208a1107c05cc8212d0c1711ccc8b218458
RaulADSI <rauramirez25@gmail.com>
2024-06-04 17:00:05 -0500

    Directiva each

--- src/main/java/com/raul/curso/springboot/web/springboot_web/controllers/UserController.java
+++ src/main/java/com/raul/curso/springboot/web/springboot_web/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.raul.curso.springboot.web.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
 
 import com.raul.curso.springboot.web.springboot_web.model.User;
 
@@ -16,5 +18,15 @@ public class UserController {
         model.addAttribute("title", "Hola Mundo Spring Boot");
         model.addAttribute("user", user);
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = Arrays.asList(new User("Pedro", "Perez"),
+                new User("Cosme", "Fulanito"),
+                new User("Kamba", "Del Carmen", "kamba@mail.com"));
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios!");
+        return "list";
     }
 }
